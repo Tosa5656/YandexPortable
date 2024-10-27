d45d90ad8ba8362dbe0fc661ab2f302090e2e83e
Tosa <127298458+Tosa5656@users.noreply.github.com>
2024-10-27 21:53:33 +0400

    Main code

---
+++ main.py
@@ -0,0 +1,8 @@
+import webview
+from webview.errors import JavascriptException
+
+def inject_adblock(window):
+    result = window.evolute_js('alert("кто прочел тот лох")')
+
+webview.create_window(title="Яндекс Музыка", url='https://passport.yandex.ru/auth/list?origin=music_button-header&retpath=https%3A%2F%2Fmusic.yandex.ru%2Fsettings%3Freqid%3D30447626117300506896817380499798786%26from-passport', width=1366, height=768, resizable=False)
+webview.start(private_mode=False)
